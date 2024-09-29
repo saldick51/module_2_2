71279057d7b411b00ecf6213e614e22fd469d79d
saldick51 <sal912@yandex.ru>
2024-09-29 09:15:39 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+my_dict={'jdhfyr':867584,'jghfk':9687564}
+print( my_dict)
+print(my_dict['jdhfyr'])
+my_dict.update({'ygehmf':887463,'kfmngh':75648})
+print(my_dict)
+print(my_dict.get('jghfk'))
+print(my_dict.get('jgrjbb',"такого ключа нет"))
+my_dict.pop('jdhfyr')
+print(my_dict)
+my_set={2,4,3,6,4,2,'utyrh',(1,2,8,8)}
+print( my_set)
+my_set.update({1,0,9})
+print(my_set)
+my_set.remove(4)
+print(my_set)
+
+
